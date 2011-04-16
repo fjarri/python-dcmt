d7e642c83eadb77f25d50043f007fb9ecb9912ba
Bogdan Opanchuk <mantihor@gmail.com>
2011-04-16 15:00:24 +1000

    dcmt/structures: using field types which match C better and added some comments about them

--- dcmt/structures.py
+++ dcmt/structures.py
@@ -42,23 +42,27 @@ def get_mt_struct(raw_data, state_len):
 
 	class mt_struct(Structure):
 		_fields_ = [
-			("aaa", c_uint),
+			("aaa", c_uint32),
 			("mm", c_int),
 			("nn", c_int),
 			("rr", c_int),
 			("ww", c_int),
-			("wmask", c_uint),
-			("umask", c_uint),
-			("lmask", c_uint),
+			("wmask", c_uint32),
+			("umask", c_uint32),
+			("lmask", c_uint32),
 			("shift0", c_int),
 			("shift1", c_int),
 			("shiftB", c_int),
 			("shiftC", c_int),
-			("maskB", c_uint),
-			("maskC", c_uint),
+			("maskB", c_uint32),
+			("maskC", c_uint32),
+
+			# filled on generator initialization
 			("i", c_int),
-			("state", POINTER(c_uint)),
-			("state_vec", c_uint * state_len) # not present in original mt_struct
+			("state", POINTER(c_uint32)),
+
+			# not present in original mt_struct
+			("state_vec", c_uint32 * state_len)
 		]
 
 	mts = mt_struct()
