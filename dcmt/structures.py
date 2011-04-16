23287bdd4e62c43adf2f2e101e7693e4c4d54935
Bogdan Opanchuk <mantihor@gmail.com>
2011-04-16 20:44:21 +1000

    dcmt/structures: removed obsolete 'todo' comment

--- dcmt/structures.py
+++ dcmt/structures.py
@@ -1,36 +1,3 @@
-# module functional interface:
-#
-# mt_struct *get_mt_parameter_st(int w, int p, uint32_t seed)
-# returns ctypes.Structure with mt_struct fields
-# checks that w and p are good and throws exception if not
-#
-# mt_struct *get_mt_parameter_id_st(int w, int p, int id, uint32_t seed)
-# returns ctypes.Structure with mt_struct fields
-# same checks as for previous one + checks that id < 65536
-#
-# mt_struct **get_mt_parameters_st(int w, int p, int start_id,
-#	int max_id, uint32_t seed)
-# returns ctypes.Array of ctypes.Structure with mt_struct fields
-# same checks as for previous one + checks that start_id and max_id are valid +
-# checks that there is no id=9 with w=31 and p=521 (known bug)
-# Note: does not return count, since user can just take len(return value)
-#
-# void sgenrand_mt(uint32_t seed, mt_struct *mts)
-# takes ctypes.Structure (from get_mt_parameter*) as a second parameter
-#
-# uint32_t genrand_mt(mt_struct *mts)
-# takes ctypes.Structure (from get_mt_parameter*) as a second parameter
-#
-#
-# additional functions:
-#
-# def get_mt_parameters_st_stripped(int w, int p, int start_id,
-#	int max_id, uint32_t seed)
-# return tuple of two values:
-# - ctypes.Structure with common parameters
-# - ctypes.Array of ctypes.Structure with unique parameters for each generator
-# same checks as for previous one
-
 from ctypes import *
 
 # Original library allocates two separate pieces of memory on a heap,
